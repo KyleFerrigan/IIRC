d80f821051702279ffcd85092e372537010d73b6
Kyle Ferrigan <kyle@ferrigan.dev>
2023-05-11 15:49:54 -0500

    Initial Commit

---
+++ IIRC/IIRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIRCApp.swift
+//  IIRC
+//
+//  Created by Triferrous on 2023-05-11.
+//
+
+import SwiftUI
+
+@main
+struct IIRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
