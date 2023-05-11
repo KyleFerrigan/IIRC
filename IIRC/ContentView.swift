241ed49eb8f3a8183a3c5c88d0cdb6a957eea082
Kyle Ferrigan <kyle@ferrigan.dev>
2023-05-11 18:49:04 -0500

    init

--- IIRC/ContentView.swift
+++ IIRC/ContentView.swift
@@ -8,16 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var server = ""
+	@State private var port = ""
+	@State private var nickname = ""
+	@State private var channel = ""
+	@State private var message = ""
+	
+	@State private var isFormFilled = false
+	
+	@ObservedObject private var client = IRCClient()
+	
+	var body: some View {
+		VStack {
+				List{
+					Section(header: Text("Connection")) {
+						TextField("Server", text: $server)
+							.disableAutocorrection(true)
+							.autocapitalization(.none)
+							.onChange(of: server) { _ in updateFormFilled() }
+						TextField("Port", text: $port)
+							.disableAutocorrection(true)
+							.autocapitalization(.none)
+							.keyboardType(.numberPad)
+							.onChange(of: port) { _ in updateFormFilled() }
+						TextField("Nickname", text: $nickname)
+							.disableAutocorrection(true)
+							.autocapitalization(.none)
+							.onChange(of: nickname) { _ in updateFormFilled() }
+						TextField("Channel", text: $channel)
+							.disableAutocorrection(true)
+							.autocapitalization(.none)
+							.onChange(of: channel) { _ in updateFormFilled() }
+						
+						Button("Connect", action: connect)
+							.foregroundColor(isFormFilled ? .blue : .gray)
+							.disabled(!isFormFilled)
+					}
+				}
+				List {
+					ForEach(client.messages, id: \.self) { message in
+						Text(message)
+					}
+				}.padding(.top, 0)
+				
+				Section {
+					HStack {
+						TextField("Message", text: $message)
+							.disableAutocorrection(true)
+							.textFieldStyle(RoundedBorderTextFieldStyle())
+						Button("Send", action: sendMessage)
+					}
+				}
+				.padding(.horizontal)
+			}
+		
+	}
+	
+	private func updateFormFilled() {
+		isFormFilled = !server.isEmpty && !port.isEmpty && !nickname.isEmpty && !channel.isEmpty
+	}
+	
+	func sendMessage() {
+		client.sendChatMessage(message, to: channel)
+		message = ""
+	}
+	
+	func connect() {
+		guard let portUInt = UInt16(port) else { return }
+		client.connect(host: server, port: portUInt, nickname: nickname, channel: channel)
+	}
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ IIRC/IRCClient.swift
@@ -0,0 +1,75 @@
+//
+//  IRCClient.swift
+//  IIRC
+//
+//  Created by Triferrous on 2023-05-11.
+//
+import Foundation
+import Network
+
+class IRCClient: ObservableObject {
+	var connection: NWConnection?
+	let queue = DispatchQueue(label: "IRC Queue")
+	var pingTimer: Timer?
+	var hostStr: String = ""
+	var hasReceivedMOTD = false
+	@Published var messages: [String] = []
+	
+	func connect(host: String, port: UInt16, nickname: String, channel: String) {
+		self.hostStr = host
+		let parameters = NWParameters.tcp
+		let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host(host), port: NWEndpoint.Port(rawValue: port) ?? NWEndpoint.Port(integerLiteral: 6667))
+		
+		connection = NWConnection(to: endpoint, using: parameters)
+		connection?.stateUpdateHandler = { newState in
+			switch newState {
+				case .ready:
+					print("Ready to send")
+					self.setupReceive()
+					self.joinChannel(nickname: nickname, channel: channel)
+				case .failed(let error):
+					print("Connection Failed: ", error)
+				default:
+					break
+			}
+		}
+		connection?.start(queue: queue)
+	}
+	
+	func joinChannel(nickname: String, channel: String) {
+		sendMessage("NICK \(nickname)\r\n")
+		sendMessage("USER \(nickname) 0 * :\(nickname)\r\n")
+		sendMessage("JOIN #\(channel)\r\n")
+		pingTimer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { _ in
+			self.sendMessage("PING :"+self.hostStr+"\r\n")
+		}
+	}
+	
+	func sendMessage(_ message: String) {
+		let data = message.data(using: .utf8)!
+		connection?.send(content: data, completion: .contentProcessed({ error in
+			if let error = error {
+				print("Send error: ", error)
+			}
+		}))
+	}
+	
+	func sendChatMessage(_ message: String, to channel: String) {
+		sendMessage("PRIVMSG #\(channel) :\(message)\r\n")
+	}
+	func setupReceive() {
+		connection?.receive(minimumIncompleteLength: 1, maximumLength: 65536) { (data, _, _, error) in
+			if let data = data, let message = String(data: data, encoding: .utf8) {
+				DispatchQueue.main.async {
+					self.messages.append(message)
+				}
+			}
+			
+			if let error = error {
+				print("Receive error: ", error)
+			} else {
+				self.setupReceive() // Continue receiving messages
+			}
+		}
+	}
+}
